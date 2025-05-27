7f73f2706e236db843125ff7ca45bb77e51245ea
ifecaro <togekk@protonmail.com>
2025-05-26 21:15:12 -0600

    feat: persist disabled choices to IndexedDB (except settings chapter), restore on reload

--- src/services/indexeddb.rs
+++ src/services/indexeddb.rs
@@ -13,4 +13,13 @@ extern "C" {
 
     #[wasm_bindgen(js_name = getChoiceFromIndexedDB)]
     pub fn get_choice_from_indexeddb(chapter_id: &str, callback: &js_sys::Function);
+
+    #[wasm_bindgen(js_name = setDisabledChoiceToIndexedDB)]
+    pub fn set_disabled_choice_to_indexeddb(paragraph_id: &str, choice_index: u32);
+
+    #[wasm_bindgen(js_name = getDisabledChoicesFromIndexedDB)]
+    pub fn get_disabled_choices_from_indexeddb(paragraph_id: &str, callback: &js_sys::Function);
+
+    #[wasm_bindgen(js_name = clearDisabledChoicesForParagraph)]
+    pub fn clear_disabled_choices_for_paragraph(paragraph_id: &str);
 } 
