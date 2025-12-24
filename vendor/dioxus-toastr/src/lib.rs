c081ce0b40d22a69f7ee81ae44ec078e67c5c69b
togekk <togekk@protonmail.com>
2025-12-24 11:18:11 -0600

    Ignore commiting vendor folder

--- vendor/dioxus-toastr/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod handle;
-mod provider;
-mod store;
-mod types;
-
-pub use handle::{use_toast, ToastHandle};
-pub use provider::{ToastProvider, ToastProviderProps};
-pub use store::ToastStore;
-pub use types::{Toast, ToastId, ToastKind, ToastOptions, ToastRequest};
